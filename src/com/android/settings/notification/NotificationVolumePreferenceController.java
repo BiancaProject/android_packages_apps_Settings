06abf1ccdc0f307210c050f409a9d37b75b26faa
Ido Ben-Hur <idoybh2@gmail.com>
2020-11-07 15:57:39 +0200

    Settings: Improve link ring & notification volume switch
    Hide the notification volume SeekBar when we don't need it
    and change the title of ring SeekBar accordingly
    was suggested by poad42
    
    Change-Id: Ibc58f7f6101f00947e06bdb60daa20890365f40d
    [nurkeinneid] Switch to a custom string to accommodate translations
    Signed-off-by: NurKeinNeid <mralexman3000@gmail.com>

--- src/com/android/settings/notification/NotificationVolumePreferenceController.java
+++ src/com/android/settings/notification/NotificationVolumePreferenceController.java
@@ -30,10 +30,12 @@ import android.os.Message;
 import android.os.ServiceManager;
 import android.os.Vibrator;
 import android.service.notification.NotificationListenerService;
+import android.provider.Settings;
 import android.text.TextUtils;
 import android.util.Log;
 
 import androidx.lifecycle.OnLifecycleEvent;
+import androidx.preference.PreferenceScreen;
 
 import com.android.internal.annotations.VisibleForTesting;
 import com.android.settings.R;
@@ -49,6 +51,7 @@ public class NotificationVolumePreferenceController extends VolumeSeekBarPrefere
 
     private static final String TAG = "NotificationVolumePreferenceController";
     private static final String KEY_NOTIFICATION_VOLUME = "notification_volume";
+    private static final String KEY_VOLUME_LINK_NOTIFICATION = "volume_link_notification";
 
     private Vibrator mVibrator;
     private int mRingerMode = AudioManager.RINGER_MODE_NORMAL;
@@ -106,6 +109,19 @@ public class NotificationVolumePreferenceController extends VolumeSeekBarPrefere
         return mContext.getResources().getBoolean(R.bool.config_show_notification_volume)
                 && !mHelper.isSingleVolume()
                 ? AVAILABLE : UNSUPPORTED_ON_DEVICE;
+    }
+
+    @Override
+    public void displayPreference(PreferenceScreen screen) {
+        super.displayPreference(screen);
+        if (getAvailabilityStatus() == UNSUPPORTED_ON_DEVICE) {
+            return;
+        }
+        VolumeSeekBarPreference notificationVolume =
+                (VolumeSeekBarPreference) screen.findPreference(KEY_NOTIFICATION_VOLUME);
+        boolean linked = Settings.Secure.getInt(mContext.getContentResolver(),
+                KEY_VOLUME_LINK_NOTIFICATION, 1) == 1;
+        notificationVolume.setVisible(linked ? false : true);
     }
 
     @Override
