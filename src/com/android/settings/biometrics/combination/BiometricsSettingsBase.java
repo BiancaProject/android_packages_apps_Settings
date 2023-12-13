CalyxOS/platform_packages_apps_Settings

Enable more featureful biometrics settings screen
* Enable AOSP's combined biometric settings screen for all devices.
  This new section offers toggles for whether biometrics can unlock
  the device or solely be used by apps. It takes the place of
  individual biometric unlock options and acts as a container for
  the face and fingerprint unlock options.

* Update preference and section title to be generic.

* Remove the "Ways to unlock" and "Use face or fingerprint to"
  categories and the intro text.

Co-authored-by: Oliver Scott <olivercscott@gmail.com>
Co-authored-by: Chirayu Desai <chirayu@calyxinstitute.org>
Issue: calyxos#1165
Change-Id: Ib188cabb762bd30036682f2e4248597b62e45cb0

src/com/android/settings/biometrics/combination/BiometricsSettingsBase.java
@@ -357,7 +357,8 @@ public abstract class BiometricsSettingsBase extends DashboardFragment {
         final ChooseLockSettingsHelper.Builder builder =
                 new ChooseLockSettingsHelper.Builder(getActivity(), this)
                         .setRequestCode(CONFIRM_REQUEST)
-                        .setTitle(getString(R.string.security_settings_biometric_preference_title))
+                        .setTitle(getString(
+                                R.string.security_settings_biometric_preference_title_calyx))
                         .setRequestGatekeeperPasswordHandle(true)
                         .setForegroundOnly(true)
                         .setReturnCredentials(true);

BiancaProject/android_packages_apps_Settings

src/com/android/settings/biometrics/combination/BiometricsSettingsBase.java
@@ -370,7 +370,8 @@ public abstract class BiometricsSettingsBase extends DashboardFragment {
         final ChooseLockSettingsHelper.Builder builder =
                 new ChooseLockSettingsHelper.Builder(getActivity(), this)
                         .setRequestCode(CONFIRM_REQUEST)
-                        .setTitle(getString(R.string.security_settings_biometric_preference_title))
+                        .setTitle(getString(
+                                R.string.security_settings_biometric_preference_title_custom))
                         .setRequestGatekeeperPasswordHandle(true)
                         .setForegroundOnly(true)
                         .setReturnCredentials(true);